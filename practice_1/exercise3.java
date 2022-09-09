3620951fa5c8a9d03f69a78e21973e915b818b94
ElkovEM <elkov.egor@gmail.com>
2022-09-09 21:47:17 +0300

    a?

---
+++ practice_1/exercise3.java
@@ -0,0 +1,63 @@
+package practice_1;
+
+import java.lang.reflect.Array;
+import java.util.Random;
+
+/*
+Написать программу,
+в результате которой считается сумма элементов целочисленного массива с помощью
+циклов for, while, do while, результат выводится на экран.
+*/
+
+public class ex3 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        fillingArray(array);
+        SumArrayFor(array);
+        SumArrayWhile(array);
+        SumArrayDoWhile(array);
+    }
+
+    public static void fillingArray(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            Random random = new Random();
+            int rand_num = random.nextInt(101);
+            array[i] = rand_num;
+        }
+        System.out.print("Defined array (integer 1 to 100): ");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+    }
+
+    public static void SumArrayFor(int[] array) {
+        int ArraySum = 0;
+        for (int j : array) {
+            ArraySum += j;
+        }
+        System.out.println("\nSum of array through 'for':");
+        System.out.println(ArraySum);
+    }
+
+    public static void SumArrayWhile(int[] array) {
+        int ArraySum = 0;
+        int i = 0;
+        while (i < array.length) {
+            ArraySum += array[i];
+            i++;
+        }
+        System.out.println("Sum of array through 'while':");
+        System.out.println(ArraySum);
+    }
+
+    public static void SumArrayDoWhile(int[] array) {
+        int ArraySum = 0;
+        int i = 0;
+        do {
+            ArraySum += array[i];
+            i++;
+        } while (i < array.length);
+        System.out.println("Sum of array through 'do while':");
+        System.out.println(ArraySum);
+    }
+}
